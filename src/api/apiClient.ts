c6394bb3d0cdca36d266a95ab0edfa69ad748b4c
miguel-merlin <mmerlin@stevens.edu>
2025-02-15 03:38:15 -0800

    feat: added axios

---
+++ src/api/apiClient.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const getApiClient = (baseUrl: string) => {
+  const apiClient = axios.create({
+    baseURL: baseUrl,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  } satisfies AxiosRequestConfig);
+  return apiClient;
+};
+
+export default getApiClient;
